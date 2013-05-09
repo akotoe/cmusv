f7cb6a97ab0a8bcb2816160d4c472b954531ed51
Edward Akoto <edward.akoto@sv.cmu.edu>
2013-05-08 21:35:42 -0700

    modified environment config to send test mails with a gmail account

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,8 +47,8 @@ CMUEducation::Application.configure do
     :port => 587,
     :authentication => :plain,
     :domain => 'smtp.gmail.com',
-    :user_name => ENV['GMAIL_SMTP_USER'] || "test@gmail.com",
-    :password => ENV['GMAIL_SMTP_PASSWORD'] || "secret",
+    :user_name => ENV['GMAIL_SMTP_USER'] || "reriedwards@gmail.com",
+    :password => ENV['GMAIL_SMTP_PASSWORD'] || "legranded",
     :enable_starttls_auto => true
 
   }
