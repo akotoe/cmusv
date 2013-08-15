70a3113ba681bb11e233e45f5c13c3bd8292086e
Edward Akoto <edward.akoto@sv.cmu.edu>
2013-08-15 12:31:35 -0700

    removed duplicate migration tasks

--- db/migrate/20130509151753_add_directory_enabled_at_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddDirectoryEnabledAtToUsers < ActiveRecord::Migration
-  def self.up
-    add_column :users, :directory_enabled_at, :timestamp
-  end
-
-  def self.down
-    remove_column :users, :directory_enabled_at
-  end
-end
