138e4983a76802a703c070b0f6a24d773cf96317
Edward Akoto <edward.akoto@sv.cmu.edu>
2013-08-15 16:37:33 -0700

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ CMUEducation::Application.configure do
       :address => "smtp.gmail.com",
       :port => 587,
       :authentication => :plain,
-      :domain => "smtp.gmail.com",
+      :domain => ENV['GMAIL_SMTP_USER'],
       :user_name => ENV['GMAIL_SMTP_USER'],
       :password => ENV['GMAIL_SMTP_PASSWORD'],
       :enable_starttls_auto => true
