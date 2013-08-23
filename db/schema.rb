8299f9db0f359945aeb75b0c70dd12abd291046f
Edward Akoto <edward.akoto@sv.cmu.edu>
2013-08-23 14:51:15 -0700

    add new user token migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130820050158) do
+ActiveRecord::Schema.define(:version => 20130820150842) do
 
   create_table "ad_group_memberships", :force => true do |t|
     t.integer  "ad_user_id"
