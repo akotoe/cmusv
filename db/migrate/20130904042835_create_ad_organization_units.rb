194044ce70baa673b05831256bd7f4d4b07c7a22
Edward Akoto <edward.akoto@sv.cmu.edu>
2013-09-03 21:29:10 -0700

    added ad_organization_unit files

---
+++ db/migrate/20130904042835_create_ad_organization_units.rb
@@ -0,0 +1,12 @@
+class CreateAdOrganizationUnits < ActiveRecord::Migration
+  def self.up
+    create_table :ad_organization_units do |t|
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :ad_organization_units
+  end
+end
---
+++ spec/factories/ad_organization_units.rb
@@ -0,0 +1,6 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :ad_organization_unit do
+  end
+end
