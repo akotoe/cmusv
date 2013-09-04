887004233e7c2090857d3b183056ca362c7afcd2
Edward Akoto <edward.akoto@sv.cmu.edu>
2013-09-03 21:31:55 -0700

    completed ad_organization_unit files

--- app/models/ad_organization_unit.rb
+++ app/models/ad_organization_unit.rb
@@ -1,2 +1,3 @@
 class AdOrganizationUnit < ActiveRecord::Base
+  validates_presence_of :distinguishedname, :whenchanged
 end
--- spec/factories/ad_organization_units.rb
+++
@@ -1,6 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :ad_organization_unit do
-  end
-end
